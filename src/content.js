bdb4d04bbdcdfd2cff160ee54c64645018a4cf91
mellow001 <122299806+mellow001@users.noreply.github.com>
2023-01-31 13:26:44 +0530

    Add files via upload

---
+++ src/content.js
@@ -0,0 +1,36 @@
+const productData = [
+    {
+      id: 1,
+      name: "Audi Suv",
+      image:
+        "https://pluspng.com/img-png/car-png-png-file-name-car-png-hd-dimension-1195x752-image-type-png-posted-on-may-11th-2017-category-cars-transportation-tags-car-1195.png",
+      productType: "SUV Cars",
+      price: 220000,
+      rating: 5,
+      timeLeft: 27,
+      totalSales: 479,
+    },
+    {
+      id: 2,
+      name: "Audi 8",
+      image:
+        "https://purepng.com/public/uploads/large/purepng.com-yellow-audi-caraudicars-961524670899johme.png",
+      productType: "Super Cars",
+      price: 320000,
+      rating: 5,
+      timeLeft: 24,
+      totalSales: 398,
+    },
+    {
+      id: 3,
+      name: "BMW Extreme",
+      image:
+        "https://www.pngall.com/wp-content/uploads/2016/07/Car-Free-Download-PNG.png",
+      productType: "Sports Cars",
+      price: 170000,
+      rating: 4,
+      timeLeft: 21,
+      totalSales: 147,
+    },
+  ];
+    export default productData;
